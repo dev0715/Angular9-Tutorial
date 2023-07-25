f6dc05f7e2db0aed2d1389860991acf2767a42da
dev0715 <toptal126@outlook.com>
2023-07-25 03:43:39 +0300

    first tutorial - Angular 9 Example Tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloWorldService } from './service/hello-world.service';
+import { MenuComponent } from './menu/menu.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, MenuComponent, HelloWorldComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [HelloWorldService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
